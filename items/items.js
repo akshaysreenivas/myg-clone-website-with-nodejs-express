1ec88b0f4cda1212a5752a04a113b47a1e343f9f
akshay <akshaypvkeerakkal@gmail.com>
2022-11-25 10:17:37 +0530

    first commit

---
+++ items/items.js
@@ -0,0 +1,54 @@
+const mobiles = [
+    {
+      name: 'SAMSUNG Galaxy A53',
+      price: 36500,
+      imgUrl: 'https://www.myg.in/images/thumbnails/460/460/detailed/34/peach2.jpg',
+      desc: '256 GB/8GB RAM | Peach'
+    },
+    {
+      name: 'VIVO Y16',
+      price: 12500,
+      imgUrl: 'https://www.myg.in/images/thumbnails/460/460/detailed/34/dg2_8s0w-1k.jpg',
+      desc: '64 GB/4GB RAM | Drizzling gold'
+    },
+    {
+      name: 'VIVO Y22',
+      price: 14000,
+      imgUrl: 'https://www.myg.in/images/thumbnails/460/460/detailed/34/mv2_qcdc-pz.jpg',
+      desc: '256 GB/8GB RAM | Star Lit blue'
+    },
+    {
+      name: 'itel A27',
+      price: 6500,
+      imgUrl: 'https://www.myg.in/images/thumbnails/460/460/detailed/34/cb1.jpg',
+      desc: '32 GB/3GB RAM | Crystal Blue'
+    },
+    {
+      name: 'Redmi Note 11 s',
+      price: 18999,
+      imgUrl: 'https://www.myg.in/images/thumbnails/460/460/detailed/32/redmi11.jpg',
+      desc: '128 GB/8GB RAM | Horizon Blue'
+    },
+    {
+      name: 'iPhone 14',
+      price: 896500,
+      imgUrl: 'https://www.myg.in/images/thumbnails/460/460/detailed/34/str1.jpg',
+      desc: '256 GB/8GB RAM | Starlight'
+    },
+    {
+      name: 'iPhone 14 Plus',
+      price: 99900,
+      imgUrl: 'https://www.myg.in/images/thumbnails/460/460/detailed/34/bl1.jpg',
+      desc: '256 GB/8GB RAM | Blue'
+    },
+    {
+      name: 'Galaxy A03 Core',
+      price: 7500,
+      imgUrl: 'https://www.myg.in/images/thumbnails/460/460/detailed/33/SAMSUNG_Galaxy_A03_Core_Green-1.jpg',
+      desc: '32 GB/3GB RAM | Mint Green'
+    },
+  ]
+   
+  
+  module.exports={mobiles}
+  
